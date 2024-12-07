5c0d8d55974bc949a5e43136e3c1f3fe51c8a07d
Наталья <korolkov.01@bk.ru>
2024-12-07 21:04:46 +0300

    homework25

---
+++ src/main/java/org/skypro/collect/basket/service/CollectBasketServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.collect.basket.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectBasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectBasketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/collect/basket/service/CollectBasketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.collect.basket.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectBasketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
